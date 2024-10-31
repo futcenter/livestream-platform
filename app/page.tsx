9e4c35e96c6c6cca33c8eff5a6d174633cabf094
Manuel Lopez <ma3xcodes@gmail.com>
2024-10-31 14:10:18 -0600

    FIx nav toggler

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+"use client"
+import React, { useRef, useState } from "react";
 import Link from "next/link";
 
 const AppPage = () => {
+    const [isToggled, setIsToggled] = useState(false);
+    const navlinks = useRef(null);
+    const hamburger = useRef(null);
+    const layer = useRef(null);
     return <>
         <header>
             <nav className="absolute z-10 w-full border-b border-black/5 dark:border-white/5 lg:border-transparent">
@@ -16,14 +21,20 @@ const AppPage = () => {
                             </a>
 
                             <div className="relative flex max-h-10 items-center lg:hidden">
-                                <button aria-label="humburger" id="hamburger" className="relative -mr-6 p-6">
+                                <button aria-label="humburger" id="hamburger" ref={hamburger} className={`relative -mr-6 p-6 ${isToggled && "toggled"}`} onClick={() => {
+                                    setIsToggled(!isToggled)
+                                }}>
                                     <div aria-hidden="true" id="line" className="m-auto h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300"></div>
                                     <div aria-hidden="true" id="line2" className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 transition duration-300 dark:bg-gray-300"></div>
                                 </button>
                             </div>
                         </div>
-                        <div id="navLayer" aria-hidden="true" className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-gray-900/70 lg:hidden"></div>
-                        <div id="navlinks" className="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none">
+                        <div id="navLayer" ref={layer} aria-hidden="true" className={`fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-gray-900/70 lg:hidden 
+                            ${isToggled && "origin-top scale-y-100"}    
+                        `}></div>
+                        <div id="navlinks" ref={navlinks} className={`invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none 
+                            ${isToggled && "!visible !scale-100 !opacity-100 !lg:translate-y-0"}
+                        `}>
                             <div className="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0">
                                 <ul className="flex flex-col gap-6 tracking-wide lg:flex-row lg:gap-0 lg:text-sm">
                                     
