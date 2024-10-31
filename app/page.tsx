2c2164d66aa0941606fc6c7e765e6347b0e4ba02
Manuel Lopez <ma3xcodes@gmail.com>
2024-10-31 14:51:12 -0600

    Change icon to live button

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ const AppPage = () => {
                         <div id="navLayer" aria-hidden="true" className={`fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 dark:bg-gray-900/70 lg:hidden 
                             ${isToggled && "origin-top scale-y-100"}    
                         `}></div>
-                        <div id="navlinks" className={`invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none 
+                        <div id="navlinks" className={`invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 dark:shadow-none lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none 
                             ${isToggled && "!visible !scale-100 !opacity-100 !lg:translate-y-0"}
                         `}>
                             <div className="w-full text-gray-600 dark:text-gray-200 lg:w-auto lg:pr-4 lg:pt-0">
@@ -40,9 +40,9 @@ const AppPage = () => {
                             </div>
 
                             <div className="mt-12 lg:mt-0">
-                                <Link href={`/live`} className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-orange-500 hover:before:bg-orange-600 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
-                                    <span className="relative text-sm font-semibold text-white">
-                                        En vivo ahora 🔴
+                                <Link href={`/live`} className="relative flex h-9 w-full items-center justify-center px-4 before:absolute before:inset-0 before:rounded-full before:bg-red-500 hover:before:bg-red-600 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max">
+                                    <span className="relative text-sm font-semibold text-white flex gap-2 items-center justify-between">
+                                        En vivo ahora <img src="/live-white.png" className="w-6 inline" />
                                     </span>
                                 </Link>
                             </div>
