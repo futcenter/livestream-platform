6b012e160989fa1e60270feaef8d5558f2430395
Manuel Lopez <ma3xcodes@gmail.com>
2024-11-03 19:32:23 -0600

    Update badge on result card

--- components/live-badge.tsx
+++ components/live-badge.tsx
@@ -1,18 +1,23 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 interface LiveBadgeProps {
   className?: string;
+  imgBadge?: boolean;
 };
 
 export const LiveBadge = ({
   className,
+  imgBadge,
 }: LiveBadgeProps) => {
   return (
     <div className={cn(
       "bg-rose-500 text-center p-0.5 px-1.5 rounded-md uppercase text-[10px] border border-background font-semibold tracking-wide",
       className,
     )}>
-      Live
+      {
+        imgBadge ? <Image src={`/live-white.png`} alt="live" width={28} height={28} /> : 'Live'
+      }
     </div>
   );
 };
--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -57,7 +57,7 @@ export const UserAvatar = ({
       </Avatar>
       {canShowBadge && (
         <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
-          <LiveBadge />
+          <LiveBadge imgBadge={true} />
         </div>
       )}
     </div>
